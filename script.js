899fbac5cf9e6b2407aceb387d2a24067441c7ed
ejett5 <minijett5@gmail.com>
2023-12-01 17:58:23 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*taking input and performing math function to find attack bonus */
+console.log("Test")
+
+
+//making base variables 
+var baseAttack = [];
+var strengthMod = [];
+var sizeMod = [];
+
+myFunction(baseAttack + strengthMod +sizeMod);{
+    return baseAttack + strengthMod + sizeMod;
+}
+
+armorFunction(10, shieldBonus, agilityMod, sizeMod);{
+    return( 10 + shieldBonus + agilityMod + sizeMod)
+}
+/*same for attack but IF range Penalty add that Else just sum */
