8d6352022291797f47d78913158599e7e938544b
ejett5 <minijett5@gmail.com>
2023-12-15 20:26:01 -0500

    add js script comments for features

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 /*taking input and performing math function to find attack bonus */
 console.log("Test")
 
+//make a d20 random generator 
 
 //make array for housing skill check bonus (from character level)
 
 
 
 
-/*same for attack but IF range Penalty add that Else just sum */
+// function that references array for skill bonus then adds random d20 and logs the results of all
 
 
-// make array for modifier levels
