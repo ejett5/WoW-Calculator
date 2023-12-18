78d43ecc4f58203f2e2bb62ed0c04bccf183f11e
ejett5 <minijett5@gmail.com>
2023-12-18 16:03:05 -0500

    made js function

--- script.js
+++ script.js
@@ -2,23 +2,33 @@
 console.log("Test")
 
 // array for housing ability modifiers; unchangable
-const array abilityModifierTable = [
-    [abScore = 0, 0],
-    [abScore = 1, -5],
-    [abScore = 2, -4],
-    [abScore = 3, -4],
-    [abScore = 4, -3],
-    [abScore = 5, -3],
-    [abScore = 6, -2],
-    [abScore = 7, -2],
-    [abScore = 8, -1],
-    [abScore = 9, -1],
+// const array abilityModifierTable = [
+//     [abScore = 0, 0],  // if 0 then no modifier
+//     [abScore = 1, -5], //if great than 0 then -1 and only increases on even level
+//     [abScore = 2, -4], //if 2 then increase every other level
+//     [abScore = 3, -4],
+//     [abScore = 4, -3],
+//     [abScore = 5, -3],
+//     [abScore = 6, -2],
+//     [abScore = 7, -2],
+//     [abScore = 8, -1],
+//     [abScore = 9, -1],
+//     [abScore = 10, 0], 
+//     [abScore = 11, 0],
+//     [abScore = 12, 1],
+//     [abScore = 13, 1], etc
 
+// function is taken in N; then return (N * 0.5).floor - 5 ; see above as reference to check it is working
 
-]
-
-
+function abilityModifierTable(){
+    //user input
+    n =getElementByID("Bob");
+}
+return((n*0.5).floor -5);
+        
+    
 //make a d20 random generator
+
 
 //make array for housing skill check bonus (from character level)
 
