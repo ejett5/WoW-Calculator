df4fb3c50b8d8f2967b39c78e16f650475c670ac
ejett5 <minijett5@gmail.com>
2023-12-18 13:35:54 -0500

    creating ability modifier table

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 /*taking input and performing math function to find attack bonus */
 console.log("Test")
 
-//make a d20 random generator 
+// array for housing ability modifiers; unchangable
+const array abilityModifierTable = [
+    [abScore = 0, 0],
+    [abScore = 1, -5],
+    [abScore = 2, -4],
+    [abScore = 3, -4],
+    [abScore = 4, -3],
+    [abScore = 5, -3],
+    [abScore = 6, -2],
+    [abScore = 7, -2],
+    [abScore = 8, -1],
+    [abScore = 9, -1],
+
+
+]
+
+
+//make a d20 random generator
 
 //make array for housing skill check bonus (from character level)
 
